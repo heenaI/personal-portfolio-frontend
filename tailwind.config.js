3f9cdec5564db157d6c2c9178e59e84d26239329
heenaI <hinaimran.mehr@gmail.com>
2025-05-16 11:31:29 +0200

    changing background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{html,ts}", 
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cream: '#F8E9A1',
+        coral: '#F76C6C',
+        lightblueBackground: '#A8D0E6',
+        denim: '#374785',
+        indigo: '#24305E',
+        textPrimary: '#24305E',   // For headings
+        textSecondary: '#374785', // For body text
+        textLight: '#ffffff',     // For dark backgrounds
+      },
+    },
   },
   plugins: [],
 }
